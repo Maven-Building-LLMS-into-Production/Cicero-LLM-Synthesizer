34e5e3449d98106faea0dd248ba8dba368d69a31
Victor Calderon <vcalderon@5x5tech.com>
2023-06-23 03:30:29 -0500

    Added main script for running the data preparation task

--- src/data_processing/prepare_dataset.py
+++ src/data_processing/prepare_dataset.py
@@ -20,21 +20,128 @@
 # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 # SOFTWARE.
 
+"""
+Module for preparing the input dataset.
+"""
+
 import logging
+from pathlib import Path
+from typing import Dict
+
+from src.classes import data_preparation as dp
+from src.utils import default_variables as dv
+from src.utils import general_utilities as gu
+
+__author__ = ["Victor Calderon"]
+__copyright__ = ["Copyright 2023 Victor Calderon"]
+__all__ = []
 
 logger = logging.getLogger(__name__)
-logging.basicConfig(level=logging.INFO)
+logging.basicConfig(
+    level=logging.INFO,
+    format="%(asctime)s [%(levelname)s]: %(message)s",
+)
+logger.setLevel(logging.INFO)
+
+
+# ---------------------------- PROJECT VARIABLES ------------------------------
+
+MODULE_DESCRIPTION = "Module for data preparation"
+MODULE_VERSION = "1.0"
+
+
+# ----------------------------- INPUT PARAMETERS ------------------------------
+
+
+def get_parser():
+    """
+    Function to get the input parameters to the script.
+    """
+    # Defining the 'parser' object to use
+    parser = gu._get_parser_obj(description=MODULE_DESCRIPTION)
+
+    # Path to the input dataset
+    parser.add_argument(
+        "--dataset-path",
+        dest="dataset_path",
+        default=dv.cicero_dataset_url,
+        type=str,
+        help="""
+        Path / URL to the input dataset.
+        [Default: '%(default)s']
+        """,
+    )
+
+    return parser.parse_args()
+
+
+# -------------------------------  FUNCTIONS ----------------------------------
+
+
+def _resolve_input_object_path(object_path: str) -> str:
+    """
+    Check whether or not the path corresponds to a local file or a URL.
+
+    Parameters
+    -------------
+    object_path : str
+        Path of the input object.
+
+    Returns
+    ----------
+    parsed_object_path : str
+        Modified / parsed version of the input object ``object_path``.
+
+    Raises
+    ------------
+    TypeError ; Error
+        This error gets raised whenever the input object is neither
+        a 'file' nor a valid 'url'.
+    """
+    object_type = gu.check_url_or_file_type(object_path=object_path)
+
+    if object_type == "unspecified":
+        msg = (
+            f">>> Unspecified data type for '{object_path}' or does not exist"
+        )
+        logger.error(msg)
+        raise TypeError(msg)
+
+    return (
+        object_path
+        if object_type == "url"
+        else str(Path(object_path).resolve())
+    )
+
 
 # ------------------------------ MAIN FUNCTIONS -------------------------------
 
 
-def main():
+def main(params_dict: Dict):
     """
     Main function to process the data.
     """
+    # Determine if the path corresponds to a file or a URL
+    params_dict["object_path"] = _resolve_input_object_path(
+        params_dict["dataset_path"]
+    )
+
+    # Showing set of input parameters
+    gu.show_params(params_dict=params_dict, logger=logger)
+
+    # Initializing input parameters
+    data_prep_obj = dp.DatasetPrep(dataset_path=params_dict["object_path"])
+    data_prep_obj.show_params()
+    clean_dataset = data_prep_obj.clean_dataset()
+
+    logger.info(f"\n>>> Raw dataset: \n{data_prep_obj.raw_dataset}\n")
+    logger.info(f"\n>>> Clean dataset: \n{clean_dataset}\n")
 
     return
 
 
 if __name__ == "__main__":
-    main()
+    # Getting input parameters
+    params_dict = vars(get_parser())
+    # Running main function
+    main(params_dict=params_dict)
